63b750270da20653cb1b8d6fc049ce680df6446f
Vincenzo D'Amore <v.damore@gmail.com>
2015-01-16 15:46:54 +0100

    first import

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer       'john q. citizen'
 maintainer_email 'john.q.citizen@test.test'
 license          'Apache 2.0'
 description      'Installs/Configures empty'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+long_description 'Installs/Configures empty'
+version          '0.4.0'
